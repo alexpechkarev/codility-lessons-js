983e6fe0fdea13ed944c6b2fc675178b67a23ad3
Alexander Pechkarev <alexpechkarev@gmail.com>
2016-09-28 14:13:12 +0100

    Create FrogJmp.js

---
+++ FrogJmp.js
@@ -0,0 +1,22 @@
+/**
+* Iterations
+* 
+* [100%] FrogJmp
+* https://codility.com/programmers/task/frog_jmp/
+* Count minimal number of jumps from position X to Y.
+*
+* 28th September 2016
+*/
+
+function solution(X,Y,D){
+
+    if(X >= Y){
+        return 0;
+    }
+    
+    if((X+D) >= Y){
+        return 1;
+    }
+    
+    return Math.ceil((Y - X)/D);
+}
